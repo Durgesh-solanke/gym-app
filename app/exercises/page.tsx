14c037d7d7fb6c85feb16ef3f7d3c74abee35900
Durgesh <rndurgesh143@gmail.com>
2026-03-14 17:29:25 +0530

    vercel deployment error2

--- app/exercises/page.tsx
+++ app/exercises/page.tsx
@@ -3,6 +3,7 @@ import { ExerciseTable } from "@/components/exercises/ExerciseTable";
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/lib/auth";
 import { redirect } from "next/navigation";
+export const dynamic = "force-dynamic";
 
 export default async function ExercisesPage() {
   const session = await getServerSession(authOptions);
--- app/log/page.tsx
+++ app/log/page.tsx
@@ -4,6 +4,7 @@ import { ExerciseLogCard } from "@/components/log/ExerciseLogCard";
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/lib/auth";
 import { redirect } from "next/navigation";
+export const dynamic = "force-dynamic";
 
 export default async function LogPage() {
   const session = await getServerSession(authOptions);
